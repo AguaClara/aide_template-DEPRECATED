7c1846b0fe486659e66a80a189281a263923da9e
zam7 <31140352+zam7@users.noreply.github.com>
2018-02-05 19:36:56 -0500

    Added YAML templates

---
+++ templates/design_yaml_generator.py
@@ -0,0 +1,78 @@
+import yaml
+import jinja2
+from aide_design.units import unit_registry as u
+import aide_design
+import numpy as np
+import os
+import flochydraulicfunctiontest as floctest
+
+# This will be passed in from aide_gui and represents all the variables that
+# the user has designed.
+user_parameters_dict = {"q_plant": 20 * u.L/u.s, "temp": 20 * u.degC}
+
+# These are the modules we want to make available within the template environment.
+modules_dict = {"u": u, "aide_design": aide_design, "np": np, "floctest": floctest}
+
+# We tell jinja to look within the design_templates folder
+folder_path = os.path.dirname(os.path.abspath(__file__))+"/design_templates"
+env = jinja2.Environment(
+    loader=jinja2.loaders.FileSystemLoader(folder_path),
+    trim_blocks=True,
+    lstrip_blocks=True
+)
+
+# Load the environment with the modules specified above.
+env.globals.update(modules_dict)
+
+template = env.get_template('plant.yaml.j2')
+#template = env.get_template('test.yaml.j2')
+
+# Run the Jinja engine on the template and print the resulting string
+rendered_template = template.render(user_parameters_dict)
+print("############## Here is the yaml file after Jinja has processed it: ")
+print(rendered_template)
+
+dictionary = yaml.load(rendered_template)
+print("############## The dictionary that pyYaml has loaded into Python from yaml: ")
+print(dictionary)
+
+
+# ############## Here is the yaml file after Jinja has processed it:
+# # This is a template to build an AguaClara water treatment plant.
+#
+# # Globals that define plant-wide parameters are passed in from the gui through
+# # the user_parameters_dict.
+#
+# # Unit processes are imported with context to provide access to the variables
+# # To print a unit process, include it.
+#
+# # The goal is to have a filled-out set of parameters to feed to Fusion that can
+# # tell Fusion how to build the plant.
+#
+#
+# - hp: # hydraulic parameters do not show up in the Fusion environment (like gt and q_plant)
+#   - q:
+#   - temp : 20 degC
+# - dp: # design parameters are put into Fusion... only parameters related to the physical construction.
+#   - n_sed_tanks: 4 dimensionless
+# - sed:
+#
+#     - hp :
+#       - q : 6 liter / second
+# - floc:
+#
+#     - hp :
+#       - q :
+#       - temp :
+#       - gt : (37, 0) # collision potential... a constant
+#       - hl :  40 centimeter #headloss
+#       - hs_min : 3 # min h/s ratio
+#       - hs_max : 6 # max h/s ratio
+#       - min_floc_width :  45 centimeter # for construction purposes, needs to be larger than a human hip
+#     - floc_tank :
+#       - hp :
+#         - volume :  TODO
+#       - dp :
+#         - L :  TODO
+# ############## The dictionary that pyYaml has loaded into Python from yaml:
+# [{'hp': [{'q': None}, {'temp': '20 degC'}]}, {'dp': [{'n_sed_tanks': '4 dimensionless'}]}, {'sed': [{'hp': [{'q': '6 liter / second'}]}]}, {'floc': [{'hp': [{'q': None}, {'temp': None}, {'gt': '(37, 0)'}, {'hl': '40 centimeter'}, {'hs_min': 3}, {'hs_max': 6}, {'min_floc_width': '45 centimeter'}]}, {'floc_tank': [{'hp': [{'volume': 'TODO'}]}, {'dp': [{'L': 'TODO'}]}]}]}]
---
+++ templates/flochydraulicfunctiontest.py
@@ -0,0 +1,46 @@
+from aide_design.play import*
+
+# expansion minor loss coefficient for 180 degree bend
+K_e = (1 / pc.RATIO_VC_ORIFICE**2 - 1)**2
+
+# kinematic viscosity
+nu = pc.viscosity_kinematic(15*u.degC)
+
+# inputs that should maybe go in expert inputs/materials database
+PI_HS_min = 3
+PI_HS_max = 6
+width_PVC_sheet = 1.067*u.m
+
+@u.wraps(1/u.s, [u.m, None, u.degK], False)
+def G_avg(hl, Gt, T):
+    """Return the average velocity gradient of a flocculator given head
+    loss, collision potential and temperature. """
+    G = (pc.gravity.magnitude * hl) / (Gt * pc.viscosity_kinematic(T).magnitude)
+    return G
+
+@u.wraps(u.m**3, [u.m**3/u.s, u.m, None, u.degK], False)
+def vol_floc(q_plant, hl, Gt, T):
+    """Return the total volume of the flocculator using plant flow rate, head
+    loss, collision potential and temperature.
+
+    Uses an estimation of flocculator residence time (ignoring the decrease
+    in water depth caused by head loss in the flocculator.) Volume does not take
+    into account the extra volume that the flocculator will have due to changing
+    water level caused by head loss."""
+    vol = (Gt / G_avg(hl, Gt, T))*q_plant
+    return vol.to(u.m**3)
+
+@u.wraps(u.cm, [u.m**3/u.s, u.m, None, u.degK], False)
+def width_HS_min(q_plant, hl, Gt, T):
+    """Return the minimum channel width required to achieve H/S > 3.
+
+    The channel can be wider than this, but this is the absolute minimum width
+    for a channel. The minimum width occurs when there is only one expansion per
+    baffle and thus the distance between expansions is the same as the depth of
+    water at the end of the flocculator."""
+    w = PI_HS_min*((K_e/(2 * depth_end * (G_avg(hl, Gt, T)**2) * nu))**(1/3))*q_plant/depth_end
+    return w.to(u.cm)
+
+@u.wraps(u.cm, [u.m**3/u.s, u.m, None, u.degK], False)
+def width_floc_min(q_plant, hl, Gt, T):
+    return max(width_HS_min(flow_plant, headloss_floc_BOD, Gt_BOD, T_BOD),width_min_PVC_sheet)
