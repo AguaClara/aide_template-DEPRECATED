627358ee85e132cd40d210b4be181bdb3fe35f31
fletchapin <fletchapin@gmail.com>
2018-05-02 14:45:09 -0400

    Bottom baffle classes completed

---
+++ templates/ent_floc_bafle_support.py
@@ -0,0 +1,10 @@
+from aide_design.units import unit_registry as u
+from aide_render.builder_classes import DP, HP
+
+
+class EntFlocBaffleSupport:
+    """This is a entrance tank, flocculator, and baffle support class.
+    It's called by the flocculator class so that the hierarchy of
+    objects in Python is the same as in Fusion.
+
+    """
